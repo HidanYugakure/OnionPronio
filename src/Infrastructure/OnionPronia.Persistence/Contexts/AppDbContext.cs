135b236f4ab5ccfc9a9880f32b8731d220d088cd
S.Fidan <146622517+HidanYugakure@users.noreply.github.com>
2025-12-22 23:48:02 +0400

    DBsET

--- src/Infrastructure/OnionPronia.Persistence/Contexts/AppDbContext.cs
+++ src/Infrastructure/OnionPronia.Persistence/Contexts/AppDbContext.cs
@@ -1,13 +1,33 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OnionPronia.Domain.Entities;
+using System.Reflection;
+using System.Reflection.Emit;
 
 
 namespace OnionPronia.Persistence.Contexts
 {
-    public class AppDbContext: DbContext
+    public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+           //modelBuilder.ApplyConfiguration(new Configuration.ProductConfiguration());
+           //modelBuilder.ApplyConfiguration(new Configuration.CategoryConfiguration());
+           //modelBuilder.ApplyConfiguration(new Configuration.TagConfiguration());
+           //modelBuilder.ApplyConfiguration(new Configuration.ProductTagConfiguration());
+
+           modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+           base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<ProductTag> ProductTags { get; set;
+        
+
+
 
         }
-    }
 }
